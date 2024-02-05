cc2ee06710d28397f90d17445db0314a35a9b4e3
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-02-05 17:21:45 -0600

    updated card text, need to add footer, style the nav links

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
       <div className="w-full h-16 backdrop-filter backdrop-blur-xl bg-opacity-20 border-b flex items-center justify-center">
         <div className="max-m-7xl w-full flex items-center justify-between p-4 ">
-          <h6 className="font-bold">Insightful</h6>
+          <h6 className="font-bold text-3xl">Vision AI</h6>
           <ul className="flex gap-8">
             <li className="hover:text-fuchsia-500 transition-colors text-xs sm:text-base"><Link href="#home">Home</Link></li>
             <li className="hover:text-fuchsia-500 transition-colors text-xs sm:text-base"><Link href="#about">About</Link></li>
--- app/page.tsx
+++ app/page.tsx
@@ -73,18 +73,18 @@ export default function Home() {
   return (
     <main className="flex min-h-screen h-fit flex-col items-center justify-center relative">
       <Navbar/>
-      <header id="home" className="flex flex-col-reverse md:flex-row w-full h-screen max-w-7xl items-center justify-cente p-8 relative overflow-x-hidden">
+      <header id="home" className="flex flex-col-reverse md:flex-row w-full h-screen max-w-7xl items-center justify-cente p-8 relative">
           <div className="w-full h-2/4 md:h-full md:w-2/5 flex flex-col justify-center items-center md:items-start gap-8">
               <div className="flex flex-col gap-2">
                 <h1 className="text-4xl font-black md:text-8xl">
-                  Insightful
+                   VisionAI
                 </h1>
                 <h2 className="text-md md:text-2xl">
-                  Start growing today!
+                Transform Your Business Today!
                 </h2>
               </div>
               <p className="max-w-md text-sm md:text-base text-zinc-500 ">
-                Insightful is an AI powered sales optimization tool that provides data-driven insights to boost sales performance.
+              VisionAI is a cutting-edge AI solutions provider, specializing in digital transformation and intelligent automation. Our platform leverages machine learning and advanced analytics to drive innovation, streamline operations, and create unparalleled customer experiences.
               </p>
               <div className="w-full flex items-center justify-center md:justify-start gap-4">
                 <button className="w-48 h-12 text-sm sm:text-base rounded bg-white text-black hover:bg-fuchsia-700 hover:text-white transition-colors">Try 7 days free</button>
