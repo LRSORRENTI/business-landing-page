7feece74a283b5c51e19208e87ab1c5c980e6d78
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-02-05 16:34:45 -0600

    added pricing card jsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Spline from "@splinetool/react-spline";
 import Navbar from "./components/Navbar";
 import Image from "next/image";
 import InfoCards from "./libs/InfoCards";
-import { LucideIcon } from "lucide-react";
+import { CheckCheck, LucideIcon } from "lucide-react";
 import { ReactElement } from "react";
 
 
@@ -40,7 +40,34 @@ interface IPricingCardProps {
 
 function PricingCard({title, price, benefits,oneliner}: IPricingCardProps) {
     return (
-        
+        <div className="h-fit w-full flex flex-col p-8 gap-8 bg-gray-900 rounded bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-20">
+            <div className="flex flex-col gap-2">
+                <div>
+                <h6 className="text-2xl">
+                {title}
+                </h6>
+                <p className="text-sm text-zinc-500">
+                  {oneliner}
+                </p>
+                </div>
+            <p className="text-4xl font-bold">
+              ${price} <span className="text-sm font-normal text-zinc-500">/ Month</span>
+            </p>
+            </div>
+            <button className="bg-fuchsia-700 rounded p-2 text-sm transition-colors hover:bg-fuchsia-800 ">Try 7 Days Free!</button>
+            <div className="flex flex-col w-full gap-4">
+              {benefits.map((benefit, i) => {
+                  return (
+                    <p key={i} className="text-sm text-zinc-500 flex items-center gap-2">
+                      <span>
+                        <CheckCheck/>
+                      </span>
+                      {benefit}
+                    </p>
+                  )
+              })}
+            </div>
+        </div>
     )
 }
 
