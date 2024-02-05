275355d25fa839c5c96e8fd831ec4116e685cbd7
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-02-05 08:26:00 -0600

    Added card info data which will go inside the main cards section

--- app/libs/infoCards.ts
+++ app/libs/infoCards.ts
@@ -1,1 +1,29 @@
-import { AlarmClock, ArrowDownNarrowWide } from "lucide-react";
+import { AlarmClock, AlarmClockOff, ArrowDownNarrowWide, ArrowUpNarrowWide, LucideIcon  } from "lucide-react";
+
+interface InfoCard {
+    title: string;
+    icon: LucideIcon;
+    bodyText: string;
+    id: number;
+}
+
+const inforCards: InfoCard[] = [
+    {
+        title: "Increased Sales",
+        bodyText: "Insightful's predictive analysis identify high-valye prospects for targeted pitches, boosting conversion rates and sales up to 20%.",
+        icon: ArrowUpNarrowWide,
+        id: 1
+    },
+    {
+        title: "No Time Wasted",
+        bodyText: "Insightful automates personalized content creation, freeing up time for revenue-focused activities and increased productivity.",
+        icon: AlarmClockOff,
+        id: 2
+    },
+    {
+        title: "Decreased Churn",
+        bodyText: "Insightful's AI load engangement and renewal tools reduce customer churn by up to 30% through consistent outreach and retention opportunities",
+        icon: ArrowUpNarrowWide,
+        id: 3
+    },
+];
