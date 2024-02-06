c5b67f2c98e11572d26bc88a04c55aade562979e
Luke Sorrenti <lrsorrenti@hotmail.com>
2024-02-06 09:05:48 -0600

    Added different spline file, looks a little cleaner

--- app/page.tsx
+++ app/page.tsx
@@ -92,8 +92,7 @@ export default function Home() {
             </div>
             <div className='w-full h-2/4 md:h-full md:w-3/5 flex items-center justify-center relative -z-10'>
           {/* <Spline className="w-full flex scale-[.25] sm:scale-[.35] lg:scale-[.5] items-center justify-center md:justify-start" scene='https://prod.spline.design/pvM5sSiYV2ivWraz/scene.splinecode'/> */}
-          <Spline className="spline-container w-full flex items-center justify-center md:justify-start" scene='/3_d_paths_lines_1_copy.spline'/>
-
+          <Spline className="w-full flex scale-[.25] sm:scale-[.35] lg:scale-[.5] items-center justify-center md:justify-start" scene='/atomic.spline'/>
         </div>
       </header>
       <section id="about" className="h-fit min-h-screen w-full flex relative items-center justify-center p-8">
